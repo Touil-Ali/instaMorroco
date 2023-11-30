285631a9438db26477be2ed24676977a9ae7fd7e
Ali <96635567+Touil-Ali@users.noreply.github.com>
2023-11-30 21:54:53 +0100

    Adding Shodcn to the project FORM + BUTTON + INPUT

--- src/App.tsx
+++ src/App.tsx
@@ -14,8 +14,8 @@ export const App = () => {
         <Routes>
             {/* public routes */}
             <Route element={<AuthLayout />}>
-            <Route path="/sign-in" element={ <SigninForm />  } />
             <Route path="/sign-up" element={ <SignupForm />  } />
+            <Route path="/sign-in" element={ <SigninForm />  } />
             </Route>
 
 
