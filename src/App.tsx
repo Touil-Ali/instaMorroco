83128a92e0d474d63180c6096831799f17017746
Ali <96635567+Touil-Ali@users.noreply.github.com>
2023-11-30 20:14:05 +0100

    Initializer

---
+++ src/App.tsx
@@ -0,0 +1,20 @@
+import { Routes, Route } from 'react-router-dom'
+import './globals.css'
+
+export const App = () => {
+
+  return (
+    <main className="flex h-screen">
+        <Routes>
+            {/* public routes */}
+            <Route path="/sign-in" element={ <SigninForm />  } />
+            
+            {/* private routes */}
+            <Route index element={ <Home />  }/>
+            
+        </Routes>
+    </main>
+  )
+}
+
+export default App
